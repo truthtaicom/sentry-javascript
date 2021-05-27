0d3ab8d5105c31b1999234b4c7724f77a40e6503
Katie Byers <lobsterkatie@gmail.com>
2021-05-27 00:41:54 -0700

    try adding SDK as file dependency

--- packages/nextjs/vercel/install-sentry-from-branch.sh
+++ packages/nextjs/vercel/install-sentry-from-branch.sh
@@ -1,6 +1,7 @@
 # SCRIPT TO INCLUDE IN A VERCEL-DEPLOYED PROJECT SO THAT IT USES A BRANCH FROM THE SDK REPO
 
 # CUSTOM INSTALL COMMAND FOR PROJECT ON VERCEL: yarn && source .sentry/install-sentry-from-branch.sh
+# CUSTOM INSTALL COMMAND FOR PROJECT ON VERCEL: source .sentry/install-sentry-from-branch.sh && yarn
 
 PROJECT_DIR=$(pwd)
 
@@ -15,66 +16,68 @@ yarn --prod false
 yarn build:es5
 cd $PROJECT_DIR
 
-# get rid of existing installed SDK packages, leaving the outer directory
-echo "rm -rf node_modules/@sentry/*"
-rm -rf node_modules/@sentry/*
+yarn add file:sentry-javascript/packages/nextjs
 
-# # cd into that directory
-# echo "cd node_modules/@sentry"
-# cd node_modules/@sentry
+# # get rid of existing installed SDK packages, leaving the outer directory
+# echo "rm -rf node_modules/@sentry/*"
+# rm -rf node_modules/@sentry/*
 
-# In `sentry-javascript/node_modules/@sentry`, all packages aside from `cli` and `webpack-plugin` are symlinks to the
-# built packages in `sentry-javascript/packages`. Copying using `-L` follows the symlinks and you end up with copies of
-# the real versions of said packages rather than copies of the symlinks. We also know that
-# `sentry-javascript/node_modules` has every dependency we need, so copying the entire `node_modules` directory kilss
-# two birds with one stone. By putting it in `<project-root>/node_modules/@sentry`, we guarantee that the resolver will
-# hit it before it hits the main `node_modules`, so even if there are duplicate packages, we know we'll get the right
-# version.
-echo "cp -r -L sentry-javascript/node_modules/ node_modules/@sentry"
-cp -r -L sentry-javascript/node_modules/ node_modules/@sentry
+# # # cd into that directory
+# # echo "cd node_modules/@sentry"
+# # cd node_modules/@sentry
 
-# The now-materialzed built packages are currently in `<project-root>/node_modules/@sentry/node_modules/@sentry`, but
-# where we really want them is `<project-root>/node_modules/@sentry`, since that's where the originals we deleted were
-echo "mv node_modules/@sentry/node_modules/@sentry/* node_modules/@sentry"
-mv node_modules/@sentry/node_modules/@sentry/* node_modules/@sentry
+# # In `sentry-javascript/node_modules/@sentry`, all packages aside from `cli` and `webpack-plugin` are symlinks to the
+# # built packages in `sentry-javascript/packages`. Copying using `-L` follows the symlinks and you end up with copies of
+# # the real versions of said packages rather than copies of the symlinks. We also know that
+# # `sentry-javascript/node_modules` has every dependency we need, so copying the entire `node_modules` directory kilss
+# # two birds with one stone. By putting it in `<project-root>/node_modules/@sentry`, we guarantee that the resolver will
+# # hit it before it hits the main `node_modules`, so even if there are duplicate packages, we know we'll get the right
+# # version.
+# echo "cp -r -L sentry-javascript/node_modules/ node_modules/@sentry"
+# cp -r -L sentry-javascript/node_modules/ node_modules/@sentry
 
-ls -l node_modules/@sentry
+# # The now-materialzed built packages are currently in `<project-root>/node_modules/@sentry/node_modules/@sentry`, but
+# # where we really want them is `<project-root>/node_modules/@sentry`, since that's where the originals we deleted were
+# echo "mv node_modules/@sentry/node_modules/@sentry/* node_modules/@sentry"
+# mv node_modules/@sentry/node_modules/@sentry/* node_modules/@sentry
 
-# # The final step is to delete the now-empty
-# echo "rm -rf node_modules/@sentry"
-# rm -rf node_modules/@sentry
+# ls -l node_modules/@sentry
 
-# for abs_package_path in ${PROJECT_DIR}/sentry-javascript/packages/*; do
+# # # The final step is to delete the now-empty
+# # echo "rm -rf node_modules/@sentry"
+# # rm -rf node_modules/@sentry
 
-# # link the built packages into project dependencies
-# for abs_package_path in sentry-javascript/packages/*; do
-#   package=$(basename $abs_package_path)
+# # for abs_package_path in ${PROJECT_DIR}/sentry-javascript/packages/*; do
 
-#   # this one will error out because it's not called @sentry/typescript, it's
-#   # called @sentry-internal/typescript, but we don't need it, so just move on
-#   if [ "$package" = "typescript" ]; then
-#     continue
-#   fi
+# # # link the built packages into project dependencies
+# # for abs_package_path in sentry-javascript/packages/*; do
+# #   package=$(basename $abs_package_path)
 
-#   echo " "
-#   echo "Linking @sentry/${package}"
+# #   # this one will error out because it's not called @sentry/typescript, it's
+# #   # called @sentry-internal/typescript, but we don't need it, so just move on
+# #   if [ "$package" = "typescript" ]; then
+# #     continue
+# #   fi
 
-#   cd $abs_package_path
-#   yarn link
+# #   echo " "
+# #   echo "Linking @sentry/${package}"
 
-#   cd $PROJECT_DIR
-#   yarn link "@sentry/$package"
-# done
+# #   cd $abs_package_path
+# #   yarn link
 
-# # These aren't in the repo and therefore have to be done separately
-# for package in "cli" "webpack-plugin"; do
+# #   cd $PROJECT_DIR
+# #   yarn link "@sentry/$package"
+# # done
 
-#   echo " "
-#   echo "Linking @sentry/${package}"
+# # # These aren't in the repo and therefore have to be done separately
+# # for package in "cli" "webpack-plugin"; do
 
-#   cd sentry-javascript/node_modules/@sentry/$package
-#   yarn link
+# #   echo " "
+# #   echo "Linking @sentry/${package}"
 
-#   cd $PROJECT_DIR
-#   yarn link "@sentry/$package"
-# done
+# #   cd sentry-javascript/node_modules/@sentry/$package
+# #   yarn link
+
+# #   cd $PROJECT_DIR
+# #   yarn link "@sentry/$package"
+# # done
