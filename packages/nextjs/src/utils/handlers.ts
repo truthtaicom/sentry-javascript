a86fb71af5006c24a739ce7b8a7e88177a9977a6
Katie Byers <lobsterkatie@gmail.com>
2021-07-05 15:46:39 -0700

    log process.env in withSentry

--- packages/nextjs/src/utils/handlers.ts
+++ packages/nextjs/src/utils/handlers.ts
@@ -14,6 +14,7 @@ type WrappedNextApiHandler = NextApiHandler;
 export const withSentry = (handler: NextApiHandler): WrappedNextApiHandler => {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   return async (req, res) => {
+    console.log(process.env);
     try {
       const currentScope = getCurrentHub().getScope();
 
